b76b54912050ba71d7889357fea85bca603fe40a
Lee Avital <leeavital@gmail.com>
2015-06-10 14:20:12 -0400

    Change name back to check-bin

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ var checkBin;
 if (process.platform  === "win32") {
     checkBin = "echo \"Windows does not have this test yet\"";
 } else {
-    checkBin = "./test/run-tests.sh";
+    checkBin = "./test/check-bin.sh";
 }
 
 module.exports = function (grunt) {
